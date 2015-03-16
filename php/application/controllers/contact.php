9b07ba471eda7fc235f2d19ab8819dc11c4bf558
arpelayo <afp.1992@gmail.com>
2015-03-16 14:23:20 -0700

    image upload for company logo of contact.

--- php/application/controllers/contact.php
+++ php/application/controllers/contact.php
@@ -34,8 +34,18 @@ class Contact extends TreeTrailController {
  public function index_post(){
   $action = $this->post('action');
   if($action === 'add'){
-  	$con = $this->post();
+    $con = $this->post();
     unset($con['action']);
+    
+    $this->load->library('upload', [
+      'file_name' => $this->generateUuid(),
+      'upload_path' => realpath(APPPATH . '../static/uploaded_photos/'),
+      'allowed_types' => 'gif|jpg|png',
+      'max_size' => '2048',
+    ]);
+    
+    $con['image_path'] = ($this->upload->do_upload('image_path')) ? $this->upload->data()['file_name'] : '';
+
     $validator = new Valitron\Validator($con);
     $validator->rule('required', ['contact_person', 'contact_number','email','organization']);
     $validator->rule('lengthMin', ['contact_person','organization'], 6);
@@ -102,4 +112,13 @@ class Contact extends TreeTrailController {
     ]);
   }
 
+  private function generateUuid() {
+    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+      mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+      mt_rand( 0, 0xffff ),
+      mt_rand( 0, 0x0fff ) | 0x4000,
+      mt_rand( 0, 0x3fff ) | 0x8000,
+      mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+    );
+  }
 }
--- php/application/views/contacts.php
+++ php/application/views/contacts.php
@@ -1,34 +1,52 @@
 {{< layout }}
 
 {{$ extra_inline_styles }}
-    .contact{
-      text-align: center;
+    #contact{
+      text-align: left;
     }
     #add{
       text-align: center;
     }
+    u:after{
+      content: "............................................................................";
+    color: transparent;
+    }
+    em{
+    color:green;
+  }
 {{/ extra_inline_styles }}
 
 {{$ extra_content}}
  <div class="content"> 
-    <div class="contact"> <h1>List of Contacts</h1></br> </div> 
+  <div class="col-lg-12">
+    <div class="col-lg-2"></div>
+    <div class="col-lg-8" id="contact"> 
+      <h1>Join Our Advocacy</h1>
+      <h3><u>Towards a <em>GREENER</em> Cebu </u></h3>
+      <h3>You may contact any of the following</h3>
+    </div> 
+    <div class="col-lg-2"></div>
+  </div>
 
+    
     <div class="col-lg-2"></div>
     <div class="col-lg-8">
  <div class="table-responsive">
-  <table class ="table table-hover">
-     <tr>
-        <th>Contact Person</th>
-        <th>Contact Number</th>
-        <th>Email</th>
-        <th>Organization</th>
-      </tr>
+  <table class ="table table-hover" id="tblContacts">
+    
   {{#contacts }}
     <tr>
-      <td>{{contact_person}}</td>
-      <td>{{contact_number}}</td>
-      <td>{{email}}</td>
-      <td>{{organization}}</td>
+      <td>
+    <div class="col-lg-3">
+      <img src="<?= base_url('/static/uploaded_photos') ?>/{{image_path}}" onerror="this.src= '<?= base_url('/static/images/plchlder.png') ?>'" id="loading" height="150" width="150" border="1px">
+    </img>
+    </div>
+    <div>
+      <strong>Contact: </strong>{{contact_person}}</br>
+      <strong>Number: </strong>{{contact_number}}</br>
+      <strong>Email: </strong>{{email}}</br>
+      <strong>Organization: </strong>{{organization}}</td>
+      </div>
     </tr>
   {{/ contacts }}
   </table></div></div>
@@ -36,5 +54,7 @@
     </div>
   </div>
 {{/ extra_content }}
+
+{{$ extra_scripts }}{{/ extra_scripts }}
 
 {{/ layout }}
