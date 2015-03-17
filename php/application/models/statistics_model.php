2e3940587a5f37769bb6c853023841c1765267f8
junichi <junichimiyahara3@yahoo.com>
2015-03-18 01:49:32 +0800

    Statistics --- changed name of badge to municipality

--- php/application/models/statistics_model.php
+++ php/application/models/statistics_model.php
@@ -3,7 +3,7 @@
 class Statistics_model extends CI_Model{
 
 	public function retrieve_all(){
-	  $this->db->select('quantity, name, id, types');
+    $this->db->select('quantity, municipality as name, id, types');
     $this->db->from('locations');
     $query = $this->db->get();
     return $query;
