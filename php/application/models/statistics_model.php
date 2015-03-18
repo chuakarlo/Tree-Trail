8b1876a5934b0e0dd4f95ec8388311b19a6cfc2b
junichi <junichimiyahara3@yahoo.com>
2015-03-18 23:47:46 +0800

    statistics bug fix typo

--- php/application/models/statistics_model.php
+++ php/application/models/statistics_model.php
@@ -5,7 +5,7 @@ class Statistics_model extends CI_Model{
 	public function retrieve_all(){
     $this->db->select('quantity, municipality as name, id, types');
     $this->db->from('locations');
-	// $this->db->where('municipality != ', '');
+	$this->db->where('municipality != ', '');
     $query = $this->db->get();
     return $query;
   }
